815e4b552ded91f60d76089462204b45dfc75f0b
Ivan <wild3dworld@gmail.com>
2021-11-10 08:16:54 +0700

    меню, функции возвращают 0)

--- Laba4/Laba4/Program.cs
+++ Laba4/Laba4/Program.cs
@@ -10,7 +10,90 @@ namespace Laba4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            double a = 0;
+            double b = 0;
+
+
+
+            //string vvedennoe = Console.ReadLine();
+            menu();
+
+            void menu()
+            {
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine("   Це калькулятор, он могёт считать");
+                Console.WriteLine();
+                Console.WriteLine("  ввод 1: задать число а| а = " + a);
+                Console.WriteLine("  ввод 2: задать число b| b = " + b);
+                Console.WriteLine("  ввод +: выполнить операцию сложения а и b");
+                Console.WriteLine("  ввод -: выполнить операцию вычитания а и b");
+                Console.WriteLine("  ввод *: выполнить операцию умножения а и b");
+                Console.WriteLine("  ввод /: выполнить операцию деления  а и b");
+                Console.WriteLine("----------------------------------------");
+
+                string i = Console.ReadLine();
+
+                switch (i)
+                {
+                    case "1":
+                        Console.WriteLine("введите число а");
+                        a = double.Parse(Console.ReadLine());
+                        Console.WriteLine("число а = " + a);
+                        menu();
+
+                        break;
+                    case "2":
+                        Console.WriteLine("введите число b");
+                        b = double.Parse(Console.ReadLine());
+                        Console.WriteLine("число b = " + b);
+                        menu();
+                        break;
+                    case "+":
+                        Console.WriteLine("a+b= " + plus(a, b));
+                        break;
+                    case "-":
+                        Console.WriteLine("a-b= " + minus(a, b));
+                        break;
+                    case "*":
+                        Console.WriteLine("a*b= " + umnoj(a, b));
+                        break;
+                    case "/":
+                        if (b == 0)
+                        {
+                            Console.WriteLine("делить на ноль низяя!!");
+                            menu();
+                        }
+                        else
+                            Console.WriteLine("a/b= " + delit(a, b));
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка! Некорректный ввод!");
+                        break;
+                }
+                //Console.Write("\nНажмите любую клавишу...");
+                //Console.ReadLine();
+                //Console.Clear();
+            }
+
+            double plus(double a1, double b2)
+            {
+                return 0;
+            }
+
+            double minus(double a1, double b2)
+            {
+                return 0;
+            }
+
+            double umnoj(double a1, double b2)
+            {
+                return 0;
+            }
+
+            double delit(double a1, double b2)
+            {
+                return 0;
+            }
         }
     }
 }
